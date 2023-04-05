daa9afac50cbcc633f98d080c765bbea603d9590
Emmanuel Baah Duodu <duoduemmanuelbaah@gmail.com>
2023-04-05 14:59:53 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
+/*class WelcomeComponent extends React.Component {
+  render() {
+    return (
+      <div className="title">
+        <h1>{this.props.name} is awesome, react works!!!!</h1>
+      </div>
+    );
+  }
+}
+
 function App() {
+  return (
+    <div className="container">
+      <WelcomeComponent name="Codetrain" />
+    </div>
+  );
+}
+
+
+export default App;*/
+
+
+
+
+
+class WelcomeComponent extends React.Component {
+  render() {
+    
+    return (
+    <div className="title">
+    <h1>Codetrain is awesome, react works!!!!</h1>;
+    </div>
+    )
+  }
+}
+
+function App() {
+ 
+  return (
+  <div className="container">
+    <WelcomeComponent />;
+   </div>
+  )
+}
+
+export default App;
+
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +72,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
